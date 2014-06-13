757611ada499f341a83b5146095624a96a76d70c
cocu <cocu.436f6375@gmail.com>
2014-06-13 22:46:41 +0900

    finished all basic features

--- src/svg-table.js
+++ src/svg-table.js
@@ -5,35 +5,42 @@ SVGTable = function (root_width, root_height, input_options) {
     var DEFAULTS = {
         cell_text: null // list(list(str))
         , cell_text_offset: [10, 23]
-        // rows
+        //--- rows
         , row_num: null // int : if cell_heights is not null, equal divide
         , cell_heights: null // list(list(int)) or null : if cell_heights is null, equal divide
         , cell_heights_is_ratio: null // if true, cell_heights use as ratio ( the_cell_height[y][x] = cell_heights[y][x]/sum(cell_heights[y]))
 
-        // columns
+        //--- columns
         , column_num: null  // int : if cell_heights is not null, equal divide
         , column_widths: null // list(int) or null : if cell_heights is null, equal divide
         , column_widths_is_ratio: null // bool : if true, cell_heights use as ratio ( the_cell_height[y][x] = cell_heights[y][x]/sum(cell_heights[y]
 
-        // column and row names
+        //--- names
+        // column names
         , column_name_list: null // iter(str) or list(str) or null : column_num
-        , column_name_cell_height: 0// int
-        , column_name_text_offset: null // (int, int) : (offsetX, offsetY)
+        , column_name_height: 0// int
+        , column_name_text_transform: '' // string
+        , column_name_widths: null //
+        // row names
         , row_name_list: null // iter(str) or list(str) or null : row-num
-        , row_name_text_offset: null // (int, int) : (offsetX, offsetY)
-        , row_name_cell_width: 0, horizontal_scale_list: null //list(int) : the int is absolute y position regardless of cell_height_is_ratio
+        , row_name_text_offset: [0, 0] // (int, int) : (offsetX, offsetY)
+        , row_name_text_transform: '' // string
+        , row_name_width: 0//
+        , row_name_heights: null // 
+        , horizontal_scale_list: null //list(int) : the int is absolute y position regardless of cell_height_is_ratio
         , vertical_scale_list: null //list(int) : the int is absolute y position regardless of cell_height_is_ratio
         , select_mode: 'horizontal' //string : 
 
-        // active
+        //-- advance
         , CLASSES: {} // css classes
-        , select_cell: null // func : is_active_cell(elem, start_col, start_row, end_col, end_row, start_posX, start_posY, end_posX, end_posY)
+        , select_cell: null // func : is_active_cell(cell, col, row, status)
+        , cell_hook: null // func : hook(cell_elem)
     };
     var SVGNS = 'http://www.w3.org/2000/svg';
     var that = this;
 
     var CLASSES = {
-        selecting: 'selecting', active: 'active', cell: 'svg_cell', table: 'svg_table'
+        selecting: 'selecting', active: 'active', cell: 'svg_cell', table: 'svg_table', row_name: 'row_name', column_name: 'column_name'
     };
 
 
@@ -70,26 +77,38 @@ SVGTable = function (root_width, root_height, input_options) {
             }
         }
     };
-    var _is_in_select = {  // select mode dictionary
-        null: args.activate_cell,
-        'rectangle': function (cell, col, row, status) {
-            // TODO
-        },
-        'horizontal': function (cell, col, row, status) {
-            var column_num = args.column_num===null?args.column_widths.length:args.column_num;
-            var x = col + row * column_num;
-            var i = status.start_col + status.start_row * column_num;
-            var j = status.end_col + status.end_row * column_num;
-            return ((i <= x && x <= j) || (j <= x && x <= i));
-        },
-        'vertical': function (cell, col, row, status) {
-            var row_num = args.row_num;
-            var x = col * row_num + row;
-            var i = status.start_col * row_num + status.start_row;
-            var j = status.end_col * row_num + status.end_row;
-            return ((i <= x && x <= j) || (j <= x && x <= i));
-        }
-    }[args.select_mode];
+    var _is_in_select = null;
+    this.set_select_mode = function (select_mode) {
+        _is_in_select = {  // select mode dictionary
+            null: args.activate_cell,//
+            'rectangle': function (cell, col, row, status) {
+                var min_col = Math.min(status.start_col, status.end_col);
+                var max_col = Math.max(status.start_col, status.end_col);
+                var min_row = Math.min(status.start_row, status.end_row);
+                var max_row = Math.max(status.start_row, status.end_row);
+                return (
+                    (min_col <= col && col <= max_col) &&
+                    (min_row <= row && row <= max_row)
+                    )
+            },
+            'horizontal': function (cell, col, row, status) {
+                var column_num = args.column_num === null ? args.column_widths.length : args.column_num;
+                var x = col + row * column_num;
+                var i = status.start_col + status.start_row * column_num;
+                var j = status.end_col + status.end_row * column_num;
+                return ((i <= x && x <= j) || (j <= x && x <= i));
+            },
+            'vertical': function (cell, col, row, status) {
+                var row_num = args.row_num;
+                var x = col * row_num + row;
+                var i = status.start_col * row_num + status.start_row;
+                var j = status.end_col * row_num + status.end_row;
+                return ((i <= x && x <= j) || (j <= x && x <= i));
+            }
+        }[select_mode]
+    };
+    this.set_select_mode(args.select_mode);
+    
     actions.toggle_class = function (class_name, status, is_add) {
         if (is_add === undefined) {
             is_add = !that.cells[status.start_col][status.start_row].hasClass(class_name);
@@ -111,7 +130,7 @@ SVGTable = function (root_width, root_height, input_options) {
         start_col: 0, end_col: 0,
         start_row: 0, end_row: 0,
         startX: 0, endX: 0,
-        startY: 0, endY: 0,
+        startY: 0, endY: 0
     };
     var event_handler_factory = function (ix, iy) {
         return function (event) {
@@ -155,19 +174,38 @@ SVGTable = function (root_width, root_height, input_options) {
     document.body.addEventListener('mouseup', actions.clear_selecting);
 
 
-    //generate row_name d:snap.svg
+    // generate names
+    this.names_root = that.svg_root.g();
+
+    if (args.column_name_list !== null && args.row_name_list !== null) {
+        (function () {
+            var cell_root = that.names_root.g()
+                    .addClass(CLASSES.table).addClass(CLASSES.cell)
+                ;
+            cell_root.rect(0, 0, args.row_name_width, args.column_name_height);
+        })();
+    }
+    // generate row_name d:snap.svg
     this.row_name_cells = new Array(this.row);
     if (args.row_name_list !== null) {
         (function () {
-            var w = args.row_name_cell_width;
-            var offsetY = 0;
-            var text_offsetX = args.row_name_text_offset[0];
-            var text_offsetY = args.row_name_text_offset[0];
-            for (var y = 0; y < args.row_num; y++) {
+            var row_names_root = that.names_root.g();
+            var w = args.row_name_width;
+            var offsetY = args.column_name_height;
+            for (var y = 0; y < args.row_name_list.length; y++) {
+                var cell_root = row_names_root.g()
+                        .addClass(CLASSES.table).addClass(CLASSES.row_name).addClass(CLASSES.cell)
+                    ;
+                cell_root.transform('translate(0,' + offsetY + ')');
+                that.row_name_cells[y] = cell_root;
                 var text = args.row_name_list[y];
-                var h = args.column_name_cell_height;
-                args.row_name_cells[y] = that.svg_root.rect(0, offsetY, w, h);
-                that.svg_root.text(text_offsetX, offsetY + text_offsetY, text);
+                var h = args.row_name_heights === null ? ((root_height - args.column_name_height) / args.row_name_list.length) : args.row_name_heights[y];
+                cell_root.rect(0, 0, w, h);
+                var transform_value = args.row_name_text_transform
+                        .replace('${width}', w).replace('${height}', h) // ISSUE#1
+                    ;
+                cell_root.text(0, 0, text).attr('transform', transform_value)
+                    .disableUserSelect();
                 offsetY += h;
             }
         })();
@@ -175,7 +213,30 @@ SVGTable = function (root_width, root_height, input_options) {
 
 
     // generate column_name 
-    // TODO
+    this.column_name_cells = new Array(this.row);
+    if (args.column_name_list !== null) {
+        (function () {
+            var column_names_root = that.names_root.g();
+            var h = args.column_name_height;
+            var offsetX = args.row_name_width;
+            for (var col = 0; col < args.column_name_list.length; col++) {
+                var cell_root = column_names_root.g()
+                        .addClass(CLASSES.table).addClass(CLASSES.column_name).addClass(CLASSES.cell)
+                    ;
+                cell_root.transform('translate(' + offsetX + ',0)');
+                that.column_name_cells[col] = cell_root;
+                var text = args.column_name_list[col];
+                var w = args.column_name_widths === null ? ((root_width - args.row_name_width) / args.column_name_list.length) : args.column_name_widths[col];
+                cell_root.rect(0, 0, w, h);
+                var transform_value = args.column_name_text_transform
+                        .replace('${width}', w).replace('${height}', h) // ISSUE#1
+                    ;
+                cell_root.text(0, 0, text).attr('transform', transform_value)
+                    .disableUserSelect();
+                offsetX += w;
+            }
+        })();
+    }
 
 
     // generate cells
@@ -192,7 +253,7 @@ SVGTable = function (root_width, root_height, input_options) {
                     : args.column_widths[col];
             } :
             function (col) {
-                return (root_width - args.row_name_cell_width) / args.column_num;
+                return (root_width - args.row_name_width) / args.column_num;
             };
         var get_height = args.row_num === null ?
             function (col, row) {
@@ -203,40 +264,33 @@ SVGTable = function (root_width, root_height, input_options) {
                     : args.cell_heights[col][row];
             } :
             function (col, row) {
-                return (root_height - args.column_name_cell_height) / args.row_num;
+                return (root_height - args.column_name_height) / args.row_num;
             };
-        var gen_rect = function (parent, col, row, offsetX, offsetY, width, height) {
-            var cell = parent
-                .rect(offsetX, offsetY, width, height)
-                .addClass(CLASSES.table).addClass(CLASSES.cell)
-                .data('col', col).data('row', row);
-            var handler = event_handler_factory(col, row);
-            cell.mousedown(handler)
-                .mouseover(handler)
-                .mouseup(handler);
-            return cell;
-        };
-        var gen_text = function (parent, col, row, offsetX, offsetY, width, height) {
 
-        };
-
-        var offsetX = 0, h, w, text, col_root, cell_root;
+        var offsetX, offsetY, h, w, text, col_root, cell_root;
+        offsetX = args.row_name_list === null ? 0 : args.row_name_width;
         var column_num = args.column_num === null ? args.column_widths.length : args.column_num
         for (var col = 0; col < column_num; col++) {
             var row_num = args.row_num === null ? args.cell_heights[col].length : args.row_num;
-            var offsetY = 0;
+            offsetY = args.column_name_list === null ? 0 : args.column_name_height;
             w = get_width(col);
             that.cells[col] = new Array(row_num);
             that.texts[col] = new Array(row_num);
             col_root = that.cells_root.group();
             for (var row = 0; row < row_num; row++) {
-                cell_root = col_root.group();
+                var handler = event_handler_factory(col, row);
+                cell_root = col_root.group()
+                    .addClass(CLASSES.table).addClass(CLASSES.cell)
+                    .data('col', col).data('row', row)
+                    .mousedown(handler).mouseover(handler).mouseup(handler);
+                that.cells[col][row] = cell_root;
                 h = get_height(col, row);
                 text = args.cell_text === null ? "" : args.cell_text[col][row];
-                that.cells[col][row] = gen_rect(cell_root, col, row, offsetX, offsetY, w, h);
+                cell_root.rect(offsetX, offsetY, w, h);
                 var text_offsetX = args.cell_text_offset === null ? 0 : args.cell_text_offset[0],
                     text_offsetY = args.cell_text_offset === null ? 0 : args.cell_text_offset[1];
-                that.texts[col][row] = cell_root.text(offsetX + text_offsetX, offsetY + text_offsetY, text);
+                that.texts[col][row] = cell_root.text(offsetX + text_offsetX, offsetY + text_offsetY, text)
+                    .disableUserSelect();
                 offsetY += get_height(col, row);
             }
             offsetX += w;
